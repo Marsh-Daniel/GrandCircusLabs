c96b7e9545d28c16668f2c5c8d31120f27b4dff8
Daniel Marsh <spud0242569@gmail.com>
2016-03-29 16:09:05 -0400

    initial commit

--- SquaredAndCubed.java
+++ SquaredAndCubed.java
@@ -3,17 +3,25 @@ package labs;
 import java.util.Scanner;
 
 public class SquaredAndCubed {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Learn your sqaures and cubes!");
-		System.out.print("Enter Your number: ");
-		int number = input.nextInt();
-		System.out.println("Number     Squared     Cubed");
-		System.out.println("======     =======     =====");
-		for(int i=1;i<number; i++){
-			System.out.println(number + "          "+((int)Math.pow(i, 2))+"          "+ ((int)Math.pow(i, 3)));
-		}
-	}
+		String answer;
+		System.out.println("Learn your squares and cubes!");
 
+		do {
+			System.out.print("\nEnter Your number: ");
+			int number = input.nextInt();
+			System.out.println("Number\tSquared\tCubed");
+			System.out.println("======\t=======\t=====");
+			for (int i = 1; i < number; i++) {
+
+				System.out.printf("%1d\t%-1d\t%-1d\n", i, ((int) Math.pow(i, 2)), ((int) Math.pow(i, 3)));
+
+			}
+			System.out.print("\nContinue? (y/n): ");
+			answer = input.next();
+		} while (answer.equalsIgnoreCase("y"));
+		System.out.println("Thank you for using this program!");
+		input.close();
+	}
 }
